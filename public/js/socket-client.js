ca805e2fc96f8faa833549754b039379d00e7cab
Bryan Lema <bryanglema2@gmail.com>
2022-11-29 00:39:38 -0500

    Delete text

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -36,7 +36,7 @@ btnEnviar.addEventListener('click', () => {
     socket.emit('enviar-mensaje',payload, ( id ) => {
         console.log("Desde el server", id)
     })
-    txtMensaje.innerHTML = ''
+    txtMensaje.value = ''
 
     
 })
